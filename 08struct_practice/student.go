7336a79055aae4749b94b6cf077b6515aaf78ba9
tao <shangyou>
2022-05-24 11:18:10 +0800

    学员管理系统

--- 08struct_practice/student.go
+++ 08struct_practice/student.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 type student struct {
-	id    int // 学号唯一
+	id    int // 学号唯一，唯一标识符
 	name  string
 	class string
 }
 
+// student的构造函数
 func newStudent(id int, name, class string) *student {
 	return &student{
-		id:    id,
-		name:  name,
-		class: class,
+		id,
+		name,
+		class,
 	}
 }
 
@@ -21,33 +22,31 @@ type studentMgr struct {
 	allStudents []*student
 }
 
-// newStudentMgr是studentMgr的构造函数
 func newStudentMgr() *studentMgr {
 	return &studentMgr{
 		allStudents: make([]*student, 0, 100),
 	}
 }
 
-// 1. 添加学生
+// 1 添加学生
 func (s *studentMgr) addStudent(newStu *student) {
 	s.allStudents = append(s.allStudents, newStu)
 }
 
-// 2. 编辑学生
-func (s *studentMgr) modifyStudent(newStu *student) {
+// 2 编辑学生信息
+func (s *studentMgr) modify(newStu *student) {
 	for i, v := range s.allStudents {
-		if newStu.id == v.id { // 当学号相同时，就表示找到了要修改的学生
-			s.allStudents[i] = newStu // 根据切片的索引直接把新学生赋值进来
+		if newStu.id == v.id { // 传入的学号相等时，就找到了该学生
+			s.allStudents[i] = newStu // 根据切片的索引直接赋值新学生
 			return
 		}
 	}
-	// 输入的学生没有找到
-	fmt.Printf("输入的学生信息有误，系统中没有学号是：%d的学生", newStu.id)
+	fmt.Printf("你输入的学生信息有误，没有找到学号为%d的学生", newStu.id)
 }
 
-// 3. 展示学生
-func (s *studentMgr) showStudent() {
+// 3 展示所有学生信息
+func (s *studentMgr) showStudentsList() {
 	for _, v := range s.allStudents {
-		fmt.Printf("学号：%d 姓名：%s 班级：%s\n", v.id, v.name, v.class)
+		fmt.Printf("学号： %d 姓名：%s 班级：%s\n", v.id, v.name, v.class)
 	}
 }

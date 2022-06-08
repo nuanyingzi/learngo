752e940a5ad3afeea22961d139e0bfd9a984fdc7
Tao i3-10105f <852947475@qq.com>
2022-06-08 23:53:00 +0800

    http

---
+++ http/get/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	/*resp, err := http.Get("http://www.baidu.com")
+	if err != nil {
+		fmt.Println("Get failed, err:", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("read from resp.Body failed.Err:%#v\n", err)
+		return
+	}
+	err = ioutil.WriteFile("baidu.html", []byte(body), 0666)
+	if err != nil {
+		fmt.Println("Write file failed, err:", err)
+		return
+	}*/
+
+	// 带参数的get请求
+}
